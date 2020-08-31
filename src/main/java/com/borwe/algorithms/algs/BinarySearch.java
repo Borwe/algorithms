60e2d9b00a2a46e7579c5022d563f04b955c449e
brian.orwe <brian.orwe@gmail.com>
2020-08-31 09:26:19 +0300

    BinarySearch update

--- src/main/java/com/borwe/algorithms/algs/BinarySearch.java
+++ src/main/java/com/borwe/algorithms/algs/BinarySearch.java
@@ -10,5 +10,24 @@ package com.borwe.algorithms.algs;
  * @author brian
  */
 public class BinarySearch {
+
+	public static int rank(int key, int[] a){
+		int lo=0;
+		int hi=a.length-1;
+
+		while(lo<=hi){
+			int mid=lo+(hi-lo)/2;
+			if(key<a[mid])
+				hi=mid-1;
+			else if(key>a[mid])
+				lo=mid+1;
+			else
+				return mid;
+		}
+		return -1;
+	}
 	
+	public static void main(String[] args) {
+		
+	}
 }
