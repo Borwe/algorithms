3ac11785acec554e30003715769e8ed00aa0ed55
brian.orwe <brian.orwe@gmail.com>
2020-11-04 16:22:55 +0300

    ex1.3.4 is done

--- src/ex1.3.4.cpp
+++ src/ex1.3.4.cpp
@@ -1,56 +1,45 @@
 #include <iostream>
-#include "stack"
-#include <stdexcept>
-#include <string>
+#include "stack.h"
 #include "custom_vals.h"
 
-class Brak {
-private:
-    char open;
-    char close;
-public:
-    Brak(const char &c){
-        switch(c){
-            case '[':
-            case ']':
-                open='[';
-                close=']';
-                break;
-            
-            case '(':
-            case ')':
-                open='(';
-                close=')';
-                break;
+DataHandler::Bool evaluate(const std::string &val){
 
-            case '{':
-            case '}':
-                open='{';
-                close='}';
-                break;
-            default:
-                throw std::runtime_error("Unkown character passed");
+    //have a stack to hold parenthisis once pushed
+    Stack<char> params;
+
+
+    for(char c:val){
+        if(c=='[' || c=='(' || c=='{' ){
+            params.push(c);
+        }else{
+            if(params.isEmpty()){
+                return false;
+            }
+
+            char item=params.peek();
+            if(c==')' && item!='(' ||
+                    c==']' && item!='[' ||
+                    c=='}' && item!='{')
+                return false;
+            else if(c==')' && item=='(' ||
+                    c==']' && item=='[' ||
+                    c=='}' && item=='{')
+                params.pop();
         }
     }
 
-    DataHandler::Bool isClose(const char &c){
-        return close==c;
+    if(params.isEmpty()==false){
+        return false;
     }
-
-    DataHandler::Bool isOpen(const char &c){
-        return open==c;
-    }
-};
-
-DataHandler::Bool evaluate(const std::string &val){
-    return false;
+    return true;
 }
+
 
 int main(){
     std::string val1="[()]{}{[()()]()}"; //should pass
     std::string val2="[(])"; //should fail
 
-    std::cout<<val1<<" has: "<<evaluate(val1)<<"\n";
-    std::cout<<val2<<" has: "<<evaluate(val2)<<"\n";
+    std::cout<<val1<<" has proper matching: "<<evaluate(val1)<<"\n";
+    std::cout<<val2<<" has proper matching: "<<evaluate(val2)<<"\n";
     return 0;
 }
