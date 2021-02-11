2b2dbfca864fc424522d67590592b7c5cfd2e139
brian.orwe <brian.orwe@gmail.com>
2021-02-11 15:33:26 +0300

    Start ex 1_3_19

---
+++ src/main/java/com/borwe/algorithms/exercises/Ex1_3_19.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.borwe.algorithms.exercises;
+
+/**
+ *
+ * @author brian
+ */
+public class Ex1_3_19 {
+
+	public static void main(String[] args) {
+		System.out.println("YOLOBITCHES");
+	}
+}
