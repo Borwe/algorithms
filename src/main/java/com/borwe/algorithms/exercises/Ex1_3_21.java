e9fe6ae4274647a46b2f872d3c274cace5e71caa
brian.orwe <brian.orwe@gmail.com>
2021-02-15 21:05:49 +0300

    Startin Ex1.3.21

---
+++ src/main/java/com/borwe/algorithms/exercises/Ex1_3_21.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.borwe.algorithms.exercises;
+
+import com.borwe.algorithms.algs.data.Printer;
+
+/**
+ *
+ * @author brian
+ */
+public class Ex1_3_21 {
+	public static void main(String[] args) {
+		Printer.println("YOLO");
+	}
+}
