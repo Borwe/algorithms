e26c613444a257cedb0ee44af32e33a0556821fd
brian.orwe <brian.orwe@gmail.com>
2021-02-16 15:38:43 +0300

    Updated Printer functionality to print from arrays

--- include/printer.h
+++ include/printer.h
@@ -3,7 +3,10 @@
 
 
 #include <iostream>
+#include <ostream>
+#include <sstream>
 #include <string>
+#include <cstring>
 
 namespace DataHandler{
 
@@ -19,12 +22,41 @@ namespace DataHandler{
         template<typename T>
         static Printer print(T &obj){
             std::cout<<obj;
-            return printer;
+            return Printer::printer;
         }
 
         template<typename T>
         static Printer println(T &obj){
             std::cout<<obj<<std::endl;
+            return Printer::printer;
+        }
+
+        static Printer println(int argc,char **argv){
+            print(argc,argv);
+            std::cout<<std::endl;
+            return Printer::printer;
+        }
+
+        static Printer  print(int argc,char **argv){
+            std::stringstream stream("");
+            for(int i=0;i<argc;i++){
+                stream<<argv[i];
+                stream<<", ";
+            }
+
+            std::string result=stream.str();
+
+            //we remove the xtra ", " at the end
+            if(argc>0){
+                char finalResult[result.size()-2];
+                const char *src=result.c_str();
+                memcpy(finalResult,src,result.size()-2);
+                finalResult[result.size()-3]='\0';
+                result=finalResult;
+            }
+
+            std::cout<<result;
+            return Printer::printer;
         }
 
         static std::string promptForReturn(std::string prompt){
--- src/ex1.3.17.cpp
+++ src/ex1.3.17.cpp
@@ -1,6 +1,6 @@
 #include "printer.h"
 
 int main(int argc,char **argv){
-    DataHandler::Printer::println("Boom");
+    DataHandler::Printer::print("Got: ").println(argc,argv);
     return 0;
 }
